a4876f6f6f2778d146b40659e5b333af6febe887
alisha fazal <alisha_fazal@hotmail.com>
2024-04-03 13:14:05 +0000

    Update settings and requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-alishafazal-djangoblog-oksrupv0l1c.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-alishafazal-djangoblog-kwmqlylbh0m.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
