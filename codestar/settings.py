8a4d57af8bbc74ca881780bb8220652aa0bbc7a6
alisha fazal <alisha_fazal@hotmail.com>
2024-04-08 20:55:13 +0000

    Add about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alishafazal-djangoblog-kwmqlylbh0m.ws-eu110.gitpod.io','.herokuapp.com']
 
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote',
     'blog',
+    'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
